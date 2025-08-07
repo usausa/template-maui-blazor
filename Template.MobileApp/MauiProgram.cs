5b522ce9dfafbd27a12ffef25c10d2c2b2ee9563
usausa <machi.pon@gmail.com>
2025-08-07 11:33:59 +0900

    Update baseline

--- Template.MobileApp/MauiProgram.cs
+++ Template.MobileApp/MauiProgram.cs
@@ -230,6 +230,7 @@ public static partial class MauiProgram
         config.AddComponentsScreen();
         config.AddComponentsLocation();
         config.AddComponentsSpeech();
+        config.AddCommunication();
 
         // Messenger
         config.BindSingleton<IReactiveMessenger>(ReactiveMessenger.Default);
